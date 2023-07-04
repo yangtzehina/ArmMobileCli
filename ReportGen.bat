5fdb2b96bb73232bbeefba986e099ec0d0fc80e3
杨子江 <yangtzehina@users.noreply.github.com>
2023-07-04 16:35:51 +0800

    提交Report的bat指令
    提交基础设置的指令

---
+++ ReportGen.bat
@@ -0,0 +1,11 @@
+
+
+SET StreamlineCLI = "C:\Program Files\Arm\Arm Mobile Studio 2023.2\streamline"
+
+SET CurrentApc = F:\streamlineData\20230322_Test3.apc
+
+cd "C:\Program Files\Arm\Arm Mobile Studio 2023.2\streamline"
+
+::Streamline-cli -pa --help
+
+Streamline-cli -pa F:\streamlineData\20230322_Test3.apc "@budget"
