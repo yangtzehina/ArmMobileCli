47372a3f888a3124c2ec1f643f51e00bbd8efa47
杨子江 <yangtzehina@users.noreply.github.com>
2023-07-05 16:34:24 +0800

    加入针对芯片的budget的限制

--- ReportGen.bat
+++ ReportGen.bat
@@ -2,10 +2,10 @@
 
 SET StreamlineCLI = "C:\Program Files\Arm\Arm Mobile Studio 2023.2\streamline"
 
-SET CurrentApc = F:\streamlineData\20230322_Test3.apc
+SET CurrentApc = F:\streamlineData\202300704_Test1.apc
 
 cd "C:\Program Files\Arm\Arm Mobile Studio 2023.2\streamline"
 
 ::Streamline-cli -pa --help
 
-Streamline-cli -pa F:\streamlineData\20230322_Test3.apc "@budget"
+Streamline-cli -pa F:\streamlineData\20230705_Test3.apc "@G610_30fps"
